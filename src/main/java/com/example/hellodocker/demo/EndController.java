9164e80f1ea725ae1083bc880988014919eae40f
GEEX1428 <yangkaikai@geexfinance.com>
2021-11-05 16:49:41 +0800

    project profiles run practice...

--- src/main/java/com/example/hellodocker/demo/EndController.java
+++ src/main/java/com/example/hellodocker/demo/EndController.java
@@ -1,23 +1,30 @@
 package com.example.hellodocker.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @Auther: GEEX1428
  * @Date: 2021/10/21 18:20
  * @Description:
  */
+@Slf4j
 @Controller
 @RequestMapping(value = "/end")
 public class EndController {
 
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssSSS");
+
     @ResponseBody
     @RequestMapping(path = "/perfect",method = RequestMethod.GET)
     public String getHi(){
-        System.out.println();
+        log.info("controller-docker:"+LocalDateTime.now().format(formatter));
         return "终于好啦...";
     }
 }
--- src/main/java/com/example/hellodocker/demo/HelloController.java
+++ src/main/java/com/example/hellodocker/demo/HelloController.java
@@ -1,23 +1,30 @@
 package com.example.hellodocker.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @Auther: GEEX1428
  * @Date: 2021/10/18 17:08
  * @Description:
  */
+@Slf4j
 @Controller
 @RequestMapping(value = "/controller")
 public class HelloController {
 
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssSSS");
+
     @ResponseBody
     @RequestMapping(path = "/docker",method = RequestMethod.GET)
     public String getHi(){
-        System.out.println("11111");
+        log.info("controller-docker:"+LocalDateTime.now().format(formatter));
         return "Hello World...";
     }
 
@@ -25,7 +32,7 @@ public class HelloController {
     @ResponseBody
     @RequestMapping(path = "/good",method = RequestMethod.GET)
     public String good(){
-        System.out.println("111111111111");
+        log.info("controller-good:"+LocalDateTime.now().format(formatter));
         return "a good day from nowing...";
     }
 }
